b59ca41cb1d4c4cda54aa0c3d25174a0b4003953
Teddy Wing <ted@teddywing.com>
2013-04-28 14:54:10 -0400

    Fix JS error in viggle.js

--- public/javascripts/viggle.js
+++ public/javascripts/viggle.js
@@ -6,14 +6,14 @@
 			var showInfo = JSON.parse(message);
 			if (showInfo.status !== 'success') {
 				VAPP.showModal(function(message) {}, 'Oops ...', null, 'Unable to get show info.', 'Please try again.', false);
-					return;
+				return;
 			}
 			
 			// Name of the current show
 			var showName = showInfo.data.program_data.program_title;
 			
 			VAPP.setTitleBarSubTitle(function(message) {}, showName);
-		}
+		});
 	}
 	
 	document.addEventListener('VAPPReady', onBridgeIsReady, true);
