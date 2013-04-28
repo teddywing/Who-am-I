ad3f3cbd961c4881084386cd9294e5e7a8b675c3
Teddy Wing <ted@teddywing.com>
2013-04-28 14:51:50 -0400

    Add some Viggle calls

---
+++ public/javascripts/viggle.js
@@ -0,0 +1,20 @@
+(function() {
+	function onBridgeIsReady () {
+		VAPP.setTitleBarTitle(function(message) {}, 'Who am I?');
+		
+		VAPP.getCurrentShow(function (message) {
+			var showInfo = JSON.parse(message);
+			if (showInfo.status !== 'success') {
+				VAPP.showModal(function(message) {}, 'Oops ...', null, 'Unable to get show info.', 'Please try again.', false);
+					return;
+			}
+			
+			// Name of the current show
+			var showName = showInfo.data.program_data.program_title;
+			
+			VAPP.setTitleBarSubTitle(function(message) {}, showName);
+		}
+	}
+	
+	document.addEventListener('VAPPReady', onBridgeIsReady, true);
+})();
