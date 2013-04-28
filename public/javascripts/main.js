737b4aead58a3a9cd2bcba9cef91984f453d27c0
Teddy Wing <ted@teddywing.com>
2013-04-27 20:19:29 -0400

    Render characters by calling API
    To render the characters, get character names and image locations from
    the internal API and render them using an Underscore template.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,37 @@
+// CoverFlow
 (function() {
-	var cf = new ContentFlow('character-select', {
-		reflectionColor: "#000000",
+	var initialise_content_flow = function() {
+		var cf = new ContentFlow('character-select', {
+			reflectionColor: "#000000",
 		
-		onclickActiveItem: function(item) {
-			alert('hello');
+			onclickActiveItem: function(item) {
+				alert('hello');
+			}
+		});
+		
+		return cf;
+	};
+	initialise_content_flow();
+
+	// Populate characters
+	
+	// Change template settings to use {{}} delimiters
+	_.templateSettings = {
+		interpolate: /\{\{(.+?)\}\}/g
+	};
+	
+	var $character_container = $('#character-select .flow');
+	var character_template = _.template($('#character-image-template').html());
+	$.get(
+		'/character-images',
+		function(response) {
+			var r = JSON.parse(response);
+			
+			$character_container.empty()
+			
+			for (var i = 0; i < r.characters.length; i++) {
+				$character_container.append(character_template(r.characters[i]));
+			}
 		}
-	});
+	);
 })();
--- routes/character-images.js
+++ routes/character-images.js
@@ -1,28 +1,30 @@
 module.exports = function(req, res) {
 	var data = {};
 	
-	data.character_images = [
-		{
-			image: '/images/characters-sample/homer.png',
-			name: 'Homer'
-		},
-		{
-			image: '/images/characters-sample/marge.png',
-			name: 'Marge'
-		},
-		{
-			image: '/images/characters-sample/lisa.png',
-			name: 'Lisa'
-		},
-		{
-			image: '/images/characters-sample/bart.png',
-			name: 'Bart'
-		},
-		{
-			image: '/images/characters-sample/maggie.png',
-			name: 'Maggie'
-		}
-	];
+	data.character_images = {
+		characters: [
+			{
+				image: '/images/characters-sample/homer.png',
+				name: 'Homer'
+			},
+			{
+				image: '/images/characters-sample/marge.png',
+				name: 'Marge'
+			},
+			{
+				image: '/images/characters-sample/lisa.png',
+				name: 'Lisa'
+			},
+			{
+				image: '/images/characters-sample/bart.png',
+				name: 'Bart'
+			},
+			{
+				image: '/images/characters-sample/maggie.png',
+				name: 'Maggie'
+			}
+		]
+	};
 	
 	res.render('character-images', data);
 };
